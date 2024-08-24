1a749ba0e5cb32a6e5ac94c1e6699e79c7fc77d4
Katherine Tsai <115205162+katherine84522@users.noreply.github.com>
2024-08-24 13:57:31 -0700

    staking frontend with wagmi and rainbowkit

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,15 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
+  sepolia
 } from 'wagmi/chains';
+import { http } from '@wagmi/core';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
+  projectId: '5427b5823f2692348a3e07b961b08b61',
+  chains: [sepolia],
+  transports: {
+    [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/In518Z9i2umy6Nqm0yTFE8d9p2b312Uh')
+  },
   ssr: true,
 });
